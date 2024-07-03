eb5500089e387d3d44145e11491afe2d806bd340
Bao <baohtrang@gmail.com>
2024-07-02 22:21:54 -0700

    Dependency Injection
    Learn about three types of Dependency Injection in Spring. Using Autowired, we can let beans use other bean. Creating a Construction allows spring to automaticlly added it to its ITC

--- JavaGaming2/src/main/java/com/SpringLearning/JavaGaming2/GamingApp4SpringLauncher.java
+++ JavaGaming2/src/main/java/com/SpringLearning/JavaGaming2/GamingApp4SpringLauncher.java
@@ -1,4 +1,5 @@
 package com.SpringLearning.JavaGaming2;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +11,10 @@ import com.SpringLearning.JavaGaming2.game.*;
 public class GamingApp4SpringLauncher {
     public static void main(String[] args) {
 
-        try(var context = new AnnotationConfigApplicationContext
-                (GamingApp4SpringLauncher.class);) {
-            
+        try(var context = new AnnotationConfigApplicationContext (GamingApp4SpringLauncher.class);) {
             context.getBean(GameConsole.class).Attack();
             context.getBean(GameRunner.class).run();
         }
-
-        // var marioGame = new MarioGame();
-        // var pacManGame = new PacManGame();
-        // GameRunner gameRunner = new GameRunner(pacManGame);
-        // gameRunner.run();
-
     }
 
 }
---
+++ JavaGaming2/src/main/java/com/SpringLearning/JavaGaming2/a1/GamingApp5DepInjection.java
@@ -0,0 +1,80 @@
+package com.SpringLearning.JavaGaming2.a1;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+//Business Classes
+// Dep 1
+// Dependency2
+
+@Component
+class BusinessClass {
+    //@Autowired -- Field injection
+    Dependency1 dependency1;
+    
+    Dependency2 dependency2;
+
+    public Dependency1 getDependency1() {
+        return dependency1;
+    }
+
+    @Autowired // Constructor injection don't need autowired it would still work.
+    /*
+     * It is recommended using constructor injection. Since you only need it once. 
+     */
+    public BusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Contructor injection");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    @Autowired // Setter / Getter injection
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter injection - set dependency 1");
+        this.dependency1 = dependency1;
+    }
+
+    public Dependency2 getDependency2() {
+        return dependency2;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        this.dependency2 = dependency2;
+    }
+
+    public String toString(){
+        return "Using " + dependency1 + " and " + dependency2;
+    }
+
+}
+
+@Component
+class Dependency1 {
+
+}
+
+@Component
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+public class GamingApp5DepInjection {
+    public static void main(String[] args) {
+
+        try(var context = new AnnotationConfigApplicationContext
+                (GamingApp5DepInjection.class);) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+            System.out.println(context.getBean(BusinessClass.class));
+        }
+    }
+
+}

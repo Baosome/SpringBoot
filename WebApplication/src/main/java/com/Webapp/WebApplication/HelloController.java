bea2a7fdb191de09b974bafd93d898172e6b7d29
Bao <baohtrang@gmail.com>
2024-07-09 19:46:59 -0700

    HTML mapping
    Added HTML to method return

---
+++ WebApplication/src/main/java/com/Webapp/WebApplication/HelloController.java
@@ -0,0 +1,35 @@
+package com.Webapp.WebApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/Hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! friends";
+    }
+
+    @RequestMapping("Hello-Html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> MY TITLE </title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h2>");
+        sb.append("Test");
+        sb.append("</h2>");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+        
+    }
+
+}

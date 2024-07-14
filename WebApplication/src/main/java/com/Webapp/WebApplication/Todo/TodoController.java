b7b29d93db8e520d8eb019e2c81896c2bcc4263b
Bao <baohtrang@gmail.com>
2024-07-14 00:09:02 -0700

    Add todo item to list page

--- WebApplication/src/main/java/com/Webapp/WebApplication/Todo/TodoController.java
+++ WebApplication/src/main/java/com/Webapp/WebApplication/Todo/TodoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -22,5 +24,15 @@ public class TodoController {
         List<Todo> todos = todoService.findByUsername("Bao");
         model.addAttribute("todos", todos);
         return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method=RequestMethod.GET)
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method=RequestMethod.POST)
+    public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+        return "redirect:list-todos";
     }
 }
--- WebApplication/src/main/resources/META-INF/resources/WEB-INF/jsp/Welcome.jsp
+++ WebApplication/src/main/resources/META-INF/resources/WEB-INF/jsp/Welcome.jsp
@@ -10,6 +10,7 @@
             <h4>Hey ${name}</h3>
             <div class=""><a href="list-todos">Manage your todos</a></div>
         </div>
+
         <script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
 		<script src="webjars/jquery/3.6.0/jquery.min.js"></script>
     </body>

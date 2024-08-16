aa0a37948b698b22d16dbb18d24803b8b8e9cb83
Bao <baohtrang@gmail.com>
2024-08-16 09:37:56 -0700

    Created new spring boot project. Started on authentication service and set up JSP

---
+++ StigManager/src/main/java/com/Bao/StigManager/StigManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Bao.StigManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StigManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StigManagerApplication.class, args);
+	}
+
+}

8c4412d9c757871915f609b37f5b678655c97568
Bao <baohtrang@gmail.com>
2024-09-25 11:49:53 -0700

    Made some functional and class component

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import {Component} from "react";
 
 function App() {
   return (
     <div className="App">
       My Todo Application
+        <FirstComponent></FirstComponent>
+        <ClassComponent></ClassComponent>
     </div>
   );
+}
+
+function FirstComponent(){
+    return (
+        <div className="FirstComponent">First Component</div>
+    );
+}
+
+class ClassComponent extends  Component{
+    render() {
+        return (
+            <div className="ClassComponent"> ClassComponent</div>
+        );
+    }
 }
 
 export default App;

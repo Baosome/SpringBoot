681c301874b7cd35dfdfdb2971cc1e17fe82d547
Bao <baohtrang@gmail.com>
2024-09-25 17:00:38 -0700

    Components in different files and some basic java script

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,28 +1,14 @@
 import './App.css';
-import {Component} from "react";
+
+import ComponentMain from './components/ComponentTests/ComponentMain'
 
 function App() {
   return (
     <div className="App">
       My Todo Application
-        <FirstComponent></FirstComponent>
-        <ClassComponent></ClassComponent>
+        <ComponentMain></ComponentMain>
     </div>
   );
-}
-
-function FirstComponent(){
-    return (
-        <div className="FirstComponent">First Component</div>
-    );
-}
-
-class ClassComponent extends  Component{
-    render() {
-        return (
-            <div className="ClassComponent"> ClassComponent</div>
-        );
-    }
 }
 
 export default App;

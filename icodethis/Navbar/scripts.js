af481f94f8c5d3c70f6bfbc0e42ce290044c1a1e
Bao <baohtrang@gmail.com>
2025-03-24 19:59:41 -0700

    Sidebar now pushes content on stig page

--- icodethis/Navbar/scripts.js
+++ icodethis/Navbar/scripts.js
@@ -1,6 +1,8 @@
 const navBarBtn = document.querySelector(".toggleNavBar");
 const mainNavBar = document.querySelector(".mainBar");
+const content = document.querySelector(".content");
 
 navBarBtn.addEventListener("click", ()=> {
    mainNavBar.classList.toggle("closeNav");
+   content.classList.toggle("closeContent")
 });
